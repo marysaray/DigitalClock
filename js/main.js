e1fe39d86eaa11590bb433997d707adc74079dae
Mary Saray <msaray.ms@outlook.com>
2021-06-15 19:46:26 -0700

    Completed clock functions.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 window.onload = function () {
-    currentTime();
+    allDay();
+    halfDay();
 };
-function currentTime() {
+function allDay() {
     var date = new Date();
     var hour = date.getHours();
     hour = updateTime(hour);
@@ -20,9 +21,33 @@ function currentTime() {
     document.getElementById("all-day").innerText =
         hour + " : " + min + " : " + sec;
     var time = setTimeout(function () {
-        currentTime();
+        allDay();
         1000;
     });
+}
+function halfDay() {
+    var date = new Date();
+    var hour = date.getHours();
+    hour = updateTime(hour);
+    var min = date.getMinutes();
+    min = updateTime(min);
+    var sec = date.getSeconds();
+    sec = updateTime(sec);
+    var midday = "AM";
+    midday = (hour >= 12) ? "PM" : "AM";
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12) : hour);
+    function updateTime(number) {
+        if (number < 10) {
+            return "0" + number;
+        }
+        else {
+            return number;
+        }
+    }
     document.getElementById("half-day").innerText =
         hour + " : " + min + " : " + sec;
+    var time = setTimeout(function () {
+        halfDay();
+        1000;
+    });
 }
--- ts/main.ts
+++ ts/main.ts
@@ -10,23 +10,22 @@ minutes and seconds which are stored in different variables.
 Resource: https://www.codesdope.com/blog/article/how-to-create-a-digital-clock-using-javascript/
 */
 window.onload = function() {
-    currentTime();
+    allDay();
+    halfDay();
 }
 /**
- * 
- Inside this function,
- an object of the Date Class is created 
- which allows you to call year, date, hour, minute, second and millisecond.
+ * This is for the 24HR time clock.
  */
-function currentTime() {
-    // create an new instance of date object.
-    let date = new Date();
-    let hour = date.getHours();
-    hour = updateTime(hour);
-    let min = date.getMinutes();
-    min = updateTime(min);
-    let sec = date.getSeconds();
-    sec = updateTime(sec);
+function allDay(){
+     // create an new instance of date object.
+     let date = new Date();
+     let hour = date.getHours();
+     hour = updateTime(hour);
+     let min = date.getMinutes();
+     min = updateTime(min);
+     let sec = date.getSeconds();
+     sec = updateTime(sec);
+
 /**
  * To always display the elements of time in two-digit format,
  * a 0 is appended before them whenever they are less than 10 
@@ -35,39 +34,70 @@ function currentTime() {
  * @returns the expression of the time.
  */
 function updateTime(number) {
-   if (number < 10) {
-       return "0" + number;
-   }
-   else{
-       return number;
-   }
+    if (number < 10) {
+        return "0" + number;
+    }
+    else{
+        return number;
+    }
 }
-
-    // adding time to the 24HR div
-    document.getElementById("all-day").innerText =
-                                hour + " : " + min + " : " + sec;
-    // setting the timer
-    let time = setTimeout(function(){
-        currentTime();
-        1000});
-    
+     // adding time to the 24HR div
+     document.getElementById("all-day").innerText =
+                                 hour + " : " + min + " : " + sec;
+     // setting the timer
+     let time = setTimeout(function(){
+         allDay();
+         1000});
+}
+/**
+ * This is for the 12HR time clock.
+ */
+function halfDay() {
+     // create an new instance of date object.
+     let date = new Date();
+     let hour = date.getHours();
+     hour = updateTime(hour);
+     let min = date.getMinutes();
+     min = updateTime(min);
+     let sec = date.getSeconds();
+     sec = updateTime(sec);
+ /* 
+ A variable named midday is created
+ and an initial value of "AM" is assigned to it.
+ This variable will be used to store "AM" or "PM".
+ Further, ternary operator is used to give the value "PM"
+ to the variable middayif the number of hours is greater
+ than or equal to 12 and "AM" otherwise.   
+ */
+    let midday = "AM";
+    midday = (hour >= 12) ? "PM" : "AM";
+/*
+Also, the 24-hour format of 00 : 01 : 23 at midnight
+should be displayed as 12 : 01 : 23 AM in the 12-hour format.
+For this, the number of hours 00 is replaced by 12.
+*/
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
+ /**
+  * To always display the elements of time in two-digit format,
+  * a 0 is appended before them whenever they are less than 10 
+  * using the updateTime() method.
+  * @param current time that will be changed. 
+  * @returns the expression of the time.
+  */
+ function updateTime(number) {
+    if (number < 10) {
+        return "0" + number;
+    }
+    else{
+        return number;
+    }
+}
     // adding time to the 12HR div
     document.getElementById("half-day").innerText =
-                                hour + " : " + min + " : " + sec;
+                                 hour + " : " + min + " : " + sec;
+    // setting the timer
+    let time = setTimeout(function(){
+    halfDay();
+    1000});
 }
-
-/*
-Now we have to add just three more lines of code
-and our clock will be ready.
-A variable named midday is created
-and an initial value of "AM" is assigned to it.
-This variable will be used to store "AM" or "PM".
-
-Further, 
-ternary operator is used to give the value "PM"
-to the variable middayif the number of hours is greater
-than or equal to 12 and "AM" otherwise.
-*/
-
-
 
